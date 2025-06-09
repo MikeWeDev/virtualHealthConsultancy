58c626a140c60e170d129cb3e987b21f17ad7d02
Mikejr <mikyasnegash2@gmail.com>
2025-06-09 23:59:24 +0300

    trying the previos code

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -22,53 +22,12 @@ export default function handler(req, res) {
       socket.join(roomId);
       console.log(`Socket ${socket.id} joined room: ${roomId}`);
 
-      // Let others in the room know a user joined (optional)
+      // Notify others in the room that a user joined
       socket.to(roomId).emit("user-joined");
 
-      // Broadcast 'signal' to everyone in the room, including sender
+      // Broadcast 'signal' event to everyone in the room, including sender
       socket.on("signal", (data) => {
-        io.to(roomId).emit("signal", data); // <== THIS is the key change!
-      });
-
-      socket.on("disconnect", () => {
-        console.log(`Socket ${socket.id} left room: ${roomId}`);
-        socket.to(roomId).emit("user-left");
-      });
-    });
-  });
-
-  res.end();
-}
-import { Server } from "socket.io";
-
-export default function handler(req, res) {
-  if (res.socket.server.io) {
-    console.log("Socket.io server already running");
-    res.end();
-    return;
-  }
-
-  console.log("Starting new Socket.io server...");
-
-  const io = new Server(res.socket.server, {
-    path: "/api/socket",
-  });
-
-  res.socket.server.io = io;
-
-  io.on("connection", (socket) => {
-    console.log("New socket connection:", socket.id);
-
-    socket.on("join", (roomId) => {
-      socket.join(roomId);
-      console.log(`Socket ${socket.id} joined room: ${roomId}`);
-
-      // Let others in the room know a user joined (optional)
-      socket.to(roomId).emit("user-joined");
-
-      // Broadcast 'signal' to everyone in the room, including sender
-      socket.on("signal", (data) => {
-        io.to(roomId).emit("signal", data); // <== THIS is the key change!
+        io.to(roomId).emit("signal", data);
       });
 
       socket.on("disconnect", () => {
